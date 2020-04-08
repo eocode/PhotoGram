a052f79fdc0808a152e8f7778d4a9c1d9407401a
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-04-08 17:00:40 -0500

    :apple: Complete signup app form

--- users/views.py
+++ users/views.py
@@ -3,6 +3,13 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from users.models import Profile
+
+# Exception
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
 
 def login_view(request):
     """Login view"""
@@ -19,6 +26,28 @@ def login_view(request):
 
 def signup(request):
     """Sign up view"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already exists'})
+        
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
     return render(request, 'users/signup.html')
 
 @login_required
